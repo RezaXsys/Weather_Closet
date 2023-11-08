b20cb27ad16d411a239204198e97f0507657c6d5
HenriLaq <henri.laquerbe@hotmail.com>
2023-11-08 18:00:28 +0100

    Pic name change for windows

--- Sensors/app.py
+++ Sensors/app.py
@@ -12,7 +12,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/takePicture')
 @cross_origin()
 def takePicture():
-    picName = str(datetime.now().strftime('%Y-%m-%d-%H:%M:%S')) + ".jpg"
+    picName = str(datetime.now().strftime('%Y-%m-%d-%H-%M-%S')) + ".jpg"
     print(picName)
     takePic(picName)
     savePic(picName, weather())
